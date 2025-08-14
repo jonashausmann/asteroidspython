db5ab111b295cb7cd1c4199303b61291156280c8
Jonas Hausmann <jonashausmann@icloud.com>
2025-08-14 13:37:07 -0400

    working black blank screen

--- main.py
+++ main.py
@@ -1,10 +1,22 @@
 import pygame
-import constants
+from constants import *
 
 def main():
+    pygame.init()
     print("Starting Asteroids!")
-    print(f"Screen width: {constants.SCREEN_WIDTH}")
-    print(f"Screen height: {constants.SCREEN_HEIGHT}")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    i = 1
+    while i != 0:
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
+        screen.fill("black")
+        pygame.display.flip()
 
 if __name__ == "__main__":
     main()
